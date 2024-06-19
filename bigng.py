dc44b7968ac616bf1ad9c1773fc062687a2e06a1
aliyasulfi <aliyasulfi18@gmail.com>
2024-06-20 03:48:35 +0530

    first

---
+++ bigng.py
@@ -0,0 +1,9 @@
+i=1
+while i<=100:
+    j=i%3
+    k=i%5
+if(j=!0 and k=!0):
+     print(i)
+     i=i+1
+
+
---
+++ firdt.py
@@ -0,0 +1,4 @@
+x = input("Enter your name ? ")
+print("your name is ", x)
+age=input("enter ur age")
+print("your age is ",age)
